904325aca4fd5e54e897413a7a75ae955bce0531
Mugunthan <87799495+Micoder-dev@users.noreply.github.com>
2022-05-15 13:45:00 +0530

    home frag design added with opening gmap in imgview

--- app/src/main/java/com/micoder/collegeuserapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/micoder/collegeuserapp/ui/home/HomeFragment.java
@@ -1,5 +1,7 @@
 package com.micoder.collegeuserapp.ui.home;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.micoder.collegeuserapp.R;
 import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
@@ -19,6 +22,7 @@ public class HomeFragment extends Fragment {
 
     SliderView sliderView;
     ArrayList<Integer> arrayList=new ArrayList<>();
+    private ImageView map;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -38,6 +42,21 @@ public class HomeFragment extends Fragment {
         sliderView.setSliderTransformAnimation(SliderAnimations.CUBEOUTDEPTHTRANSFORMATION);
         sliderView.setSliderAdapter(imageSlider);
         sliderView.startAutoCycle();
+
+        map=view.findViewById(R.id.map);
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMap();
+            }
+
+            private void openMap() {
+                Uri uri=Uri.parse("geo:0, 0?q=Periyar University Salem");
+                Intent intent=new Intent(Intent.ACTION_VIEW,uri);
+                intent.setPackage("com.google.android.apps.maps");
+                startActivity(intent);
+            }
+        });
 
         return view;
     }
